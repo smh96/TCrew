8b759365ecffd3cb9ddc5cffc3095c9ecdee2aaf
Sander Haug <sander_haug@hotmail.com>
2023-06-03 20:10:10 +0200

    Initial Commit

---
+++ TCrew/TCrewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCrewApp.swift
+//  TCrew
+//
+//  Created by Sander Haug on 03/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TCrewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
